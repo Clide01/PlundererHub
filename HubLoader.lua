88acfc0742f06e3c242b544242120b918ea52dd0
Clide Amiana <zyrelclideamiana@gmail.com>
2026-09-19 22:32:20 +0800

    Add script key and define ROUTES table

--- HubLoader.lua
+++ HubLoader.lua
@@ -1,4 +1,7 @@
 local HttpGet = game:HttpGet
+
+scriptkey = "keyless"
+
 local ROUTES = {
     -- Steal An Egg
     [107778070777162] = "https://api.redstoneguard.xyz/api/loader/f57732b2-b144-4aa4-8beb-80789d4ad6aa/init",
