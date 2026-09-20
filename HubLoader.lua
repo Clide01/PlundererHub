41e395d51341b69c71d1587a44874e05c35577c7
Clide Amiana <zyrelclideamiana@gmail.com>
2026-09-20 20:25:26 +0800

    Update script URL for place 107778070777162

--- HubLoader.lua
+++ HubLoader.lua
@@ -2,7 +2,7 @@ local HttpGet = game:HttpGet
 
 local ROUTES = {
     -- Steal An Egg
-    [107778070777162] = "https://api.redstoneguard.xyz/api/loader/f57732b2-b144-4aa4-8beb-80789d4ad6aa/init",
+    [107778070777162] = "https://luasnapper.xyz/files/loaders/ec7cea9c6d4640d7837d8bdbb7077cd9.lua",
 }
 
 local function loadScriptForPlace()
