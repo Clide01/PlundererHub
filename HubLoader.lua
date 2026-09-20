61711d26074ed0ac2a788de9fc3f94b3e9e9686d
Clide Amiana <zyrelclideamiana@gmail.com>
2026-09-20 20:44:50 +0800

    Update HubLoader.lua

--- HubLoader.lua
+++ HubLoader.lua
@@ -1,4 +1,4 @@
-local HttpGet = game:HttpGet
+
 
 local ROUTES = {
     -- Steal An Egg
@@ -19,6 +19,5 @@ local function loadScriptForPlace()
     if not ok then
         warn("[PlundererHub] Failed: " .. tostring(err))
     end
-end
 
 loadScriptForPlace()
