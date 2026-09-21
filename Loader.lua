dadd8e2c4b0e22d32d92bfd2524d5c72e515d7d4
Clide Amiana <zyrelclideamiana@gmail.com>
2026-09-21 20:28:21 +0800

    Update and rename HubLoader.lua to Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,7 +2,7 @@ local HttpGet = game:HttpGet
 
 local ROUTES = {
     -- Steal An Egg
-    [107778070777162] = "loadstring(game:HttpGet("https://luasnapper.xyz/files/loaders/ec7cea9c6d4640d7837d8bdbb7077cd9.lua"))()",
+    [107778070777162] = "https://api.redstoneguard.xyz/api/loader/e09ad35e-e8be-4066-8592-9b79bf2666cb/init",
 }
 
 local function loadScriptForPlace()
